df2b16d9a7b73e5a34165203fb7e18a0d08260ca
David DiBenedetto <david@njweb.solutions>
2022-09-24 12:58:09 -0400

    Pass all tests in stmnts-06.test.js
    Write return statements for fuctions to pass tests

--- src/stmnts-07.js
+++ src/stmnts-07.js
@@ -97,7 +97,7 @@ function gradeGenerator(score) {
  * ? return a string written like:
  * ? Francine got an A
  * ? David got a B
- * ? note: you have to use English grammar's correct indefinite article 
+ * ? note: you have to use English grammar's correct indefinite article
  * ? it's 'an A' (not a A) and 'an F' (not a F)
  */
 function getGrade(name, score) {
